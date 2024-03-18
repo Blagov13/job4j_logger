b09b3d89bfe150c7ad4737475817e06c4f0ed005
aleksandrblagov <blagov13rus@gmail.com>
2024-03-18 15:29:37 +0300

    Properties

--- src/main/java/ru/job4j/logger/LoggerImpl.java
+++ src/main/java/ru/job4j/logger/LoggerImpl.java
@@ -1,24 +1,44 @@
 package ru.job4j.logger;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.Properties;
 
 public class LoggerImpl implements Logger {
+
     @Override
     public void log(String message) {
-        System.out.println(message);
+        if ("FILE".equals(properties.getProperty("output.target"))) {
+            try (FileOutputStream fileOutputStream = new FileOutputStream("log.txt",
+                    Boolean.parseBoolean(properties.getProperty("file.append")))) {
+                fileOutputStream.write(message.getBytes());
+                fileOutputStream.write("\n".getBytes());
+            } catch (IOException e) {
+                System.out.println("Error");
+            }
+        } else if ("CONSOLE".equals(properties.getProperty("output.target"))) {
+            System.out.println(message);
+        } else {
+            System.out.println("No");
+        }
     }
+
+    private static void loadProperties() {
+        try (InputStream input = LoggerImpl.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                System.out.println("Not found");
+                return;
+            }
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static Properties properties = new Properties();
 
     public static void main(String[] args) {
         Logger logger = new LoggerImpl();
-        try (PrintWriter writer = new PrintWriter(new FileWriter("log.txt", true))) {
-            writer.println("Start");
-            logger.log("message write in the log");
-            writer.println("End");
-        } catch (IOException e) {
-            System.out.println("Error");
-        }
-        logger.log("message write in the console.");
+        loadProperties();
+        logger.log("message");
     }
 }
