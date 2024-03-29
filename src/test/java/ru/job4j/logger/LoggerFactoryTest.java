4c5f15c8e118623dc2df65de5899b884e19daf51
aleksandrblagov <blagov13rus@gmail.com>
2024-03-29 10:52:38 +0300

    Properties

--- src/test/java/ru/job4j/logger/LoggerFactoryTest.java
+++ src/test/java/ru/job4j/logger/LoggerFactoryTest.java
@@ -1,44 +1,44 @@
 package ru.job4j.logger;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class LoggerFactoryTest {
     @Test
-    void getInstance_returns_the_same_instance() {
-        assertSame(LoggerFactory.getInstance(), LoggerFactory.getInstance());
-    }
-
-    @Test
-    void log_message_with_ConsoleLogger() throws Exception {
-        File appProperties = new File("application.properties");
-        try (FileWriter writer = new FileWriter(appProperties)) {
+    void consoleLoggerTest(@TempDir Path tempDir) throws IOException {
+        File properties = tempDir.resolve("application.properties").toFile();
+        try (FileWriter writer = new FileWriter(properties)) {
             writer.write("appender=console\nminLevel=INFO");
         }
 
         Logger logger = LoggerFactory.getInstance();
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
 
         logger.info("message");
 
-        String outContentString = outContent.toString("UTF-8");
-        assertTrue(outContentString.contains("message"));
+        String outString = out.toString(StandardCharsets.UTF_8);
+        assertThat(outString).contains("message");
     }
 
     @Test
-    void log_message_with_FileLogger() throws Exception {
-        File appProperties = new File("application.properties");
-        try (FileWriter writer = new FileWriter(appProperties)) {
+    void fileLoggerTest(@TempDir Path tempDir) throws IOException {
+        File properties = tempDir.resolve("application.properties").toFile();
+        try (FileWriter writer = new FileWriter(properties)) {
             writer.write("appender=file\nminLevel=INFO");
         }
 
         Logger logger = LoggerFactory.getInstance();
         logger.info("message");
 
-        assertTrue(Files.readString(Paths.get("log.txt")).contains("message"));
+        File file = Paths.get("log.txt").toFile();
+        assertThat(Files.readString(file.toPath())).contains("message");
     }
 }
